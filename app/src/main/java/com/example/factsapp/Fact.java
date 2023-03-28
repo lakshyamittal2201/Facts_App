39670b5a3a9bdc97bd5518bf5a4a7353b233855e
lakshyamittal <lakshyamittal2201@gmail.com>
2023-03-28 13:32:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/factsapp/Fact.java
@@ -0,0 +1,48 @@
+package com.example.factsapp;
+
+public class Fact {
+
+    public static String[] getsFacts(){
+
+
+
+        return new String[] {
+
+                "Tigers have striped skin, not just striped fur.",
+                "Some bears build nests in trees to sunbathe and rest.",
+                "A group of jellyfish is called a smack.",
+                "The middle finger has the fastest growing nail.",
+                "Your big toe only has 2 bones and the rest have 3.",
+                "The average person takes 23,000 breaths a day.",
+                "It is illegal to run out of gas in Youngstown, Ohio.",
+                "It is not possible to tickle yourself.",
+                "Tug-of-war was an Olympic sport in the early 1900s.",
+
+                "Tigers have striped skin, not just striped fur.",
+                "Some bears build nests in trees to sunbathe and rest.",
+                "A group of jellyfish is called a smack.",
+                "The middle finger has the fastest growing nail.",
+                "Your big toe only has 2 bones and the rest have 3.",
+                "The average person takes 23,000 breaths a day.",
+                "It is illegal to run out of gas in Youngstown, Ohio.",
+                "It is not possible to tickle yourself.",
+                "Tug-of-war was an Olympic sport in the early 1900s.",
+
+                "Tigers have striped skin, not just striped fur.",
+                "Some bears build nests in trees to sunbathe and rest.",
+                "A group of jellyfish is called a smack.",
+                "The middle finger has the fastest growing nail.",
+                "Your big toe only has 2 bones and the rest have 3.",
+                "The average person takes 23,000 breaths a day.",
+                "It is illegal to run out of gas in Youngstown, Ohio.",
+                "It is not possible to tickle yourself.",
+                "Tug-of-war was an Olympic sport in the early 1900s.",
+
+                "Children grow faster during springtime."
+
+        };
+
+    }
+
+
+}
